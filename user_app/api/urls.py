72f269035bd4960f4bbeb875e6b508fc22492fb9
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-07-08 20:32:25 +0200

    refresh cookie token

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CheckUserExistsView, LogInView, RegisterView, ActivateUserView, CookieTokenObtainPairView
+from .views import CheckUserExistsView, LogInView, RegisterView, ActivateUserView, CookieTokenObtainPairView, CookieTokenRefreshView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name ="user_app"
@@ -8,7 +8,7 @@ urlpatterns =[
     path('check/', CheckUserExistsView.as_view(), name='check'),
     path('login/', LogInView.as_view(), name='login'),
     path('token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
     path('activate/', ActivateUserView.as_view(), name='activate')
 ]
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -4,8 +4,8 @@ from rest_framework import status
 from .serializers import CheckUserSerializer, LogInSerializer, RegisterSerializer, ActivateUserSerializer, CustomTokenObtainPairSerializer
 from django.contrib.auth import login
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 class CheckUserExistsView(GenericAPIView):
     permission_classes = [AllowAny]
@@ -54,7 +54,7 @@ class RegisterView(GenericAPIView):
     
     
 class ActivateUserView(GenericAPIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     serializer_class = ActivateUserSerializer
     
     def post(self, request):
@@ -85,10 +85,43 @@ class CookieTokenObtainPairView(TokenObtainPairView):
         )
         
         response.set_cookie(
-            key="refesh_token",
+            key="refresh_token",
             value=refresh,
             httponly=True,
             secure=True,
             samesite="Lax"
         )
+        return response
+    
+class CookieTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get("refresh_token")
+        
+        if refresh_token is None:
+            return Response(
+                {"details": "Refresh token not found!"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        
+        serializer = self.get_serializer(data={"refresh":refresh_token})
+        
+        try:
+            serializer.is_valid(raise_exception=True)
+        except:
+           return Response(
+                {"details": "Refresh token invalid!"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+           
+        access_token = serializer.validated_data.get("access")
+        
+        response = Response({"message": "access Token refreshed"})
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite="Lax"
+        )
+        
         return response
