d2e82063a7020d4f795e5f8f85ff0373a645afca
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-07-26 21:13:16 +0200

    implement logout

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CheckUserExistsView, RegisterView, ActivateUserView, CookieTokenObtainPairView, CookieTokenRefreshView, ForgotPasswordView, ChangePasswordView
+from .views import CheckUserExistsView, RegisterView, ActivateUserView, CookieTokenObtainPairView, CookieTokenRefreshView, ForgotPasswordView, ChangePasswordView, CookieTokenRemoveView
 
 
 app_name ="user_app"
@@ -7,6 +7,7 @@ app_name ="user_app"
 urlpatterns =[
     path('check/', CheckUserExistsView.as_view(), name='check'),
     path('login/', CookieTokenObtainPairView.as_view(), name='login'),
+    path('logout/', CookieTokenRemoveView.as_view(), name='logout'),
     path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
     path('activate/<uidb64>/<token>/', ActivateUserView.as_view(), name='activate'),
